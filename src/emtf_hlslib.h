8828f8d74e9572481115488dab730a0c007fc310
Jia Fu Low <jia.fu.low@cern.ch>
2021-07-24 00:48:41 -0400

    Add implementation 7/8
    Fetched emtf_hlslib v3.1.3

---
+++ src/emtf_hlslib.h
@@ -0,0 +1,17 @@
+#ifndef __EMTF_HLSLIB_H__
+#define __EMTF_HLSLIB_H__
+
+#include "emtf_hlslib/common.h"
+#include "emtf_hlslib/traits.h"
+#include "emtf_hlslib/types.h"
+#include "emtf_hlslib/layer_helpers.h"
+
+#include "emtf_hlslib/zoning.h"
+#include "emtf_hlslib/pooling.h"
+#include "emtf_hlslib/zonesorting.h"
+#include "emtf_hlslib/zonemerging.h"
+#include "emtf_hlslib/trkbuilding.h"
+#include "emtf_hlslib/duperemoval.h"
+#include "emtf_hlslib/fullyconnect.h"
+
+#endif  // __EMTF_HLSLIB_H__ not defined
